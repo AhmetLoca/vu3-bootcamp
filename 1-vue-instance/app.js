8ed71ac7c8dd146ccee79a5625e9a5b626e0bff1
ahmet <locaahmet@gmail.com>
2022-11-15 17:22:33 +0300

    vue instance

---
+++ 1-vue-instance/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  //property
+  data() {
+    return {
+        title:"Vue.js"
+    };
+  },
+}).mount("#app");
+
+/*
+
+Vue2 version
+
+new Vue({
+    data:{ 
+    }
+})
+
+*/
