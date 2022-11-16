e8cf4f1a2458d4adf0ec216f20974c10fa6566e6
ahmet <locaahmet@gmail.com>
2022-11-17 00:21:58 +0300

    vue instance

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -1,19 +1,40 @@
 const app = Vue.createApp({
-  //property
   data() {
     return {
-        title:"Vue.js"
+      title: "Hi",
+      description: "selam",
+      content: "Lorem ipsum dolor sit amet...",
+      url: "https://www.google.com",
+
+      context: {
+        title: "ahmetloca",
+        url: "https://www.ahmetloca.com",
+        target: "_blank",
+      },
     };
+  },
+  methods: {
+    changeTitle(title) {
+      // bu vue instance'indaki title'la erismek için "this" kullanacagım.
+      this.title = title;
+    },
   },
 }).mount("#app");
 
 /*
-
 Vue2 version
 
 new Vue({
     data:{ 
     }
 })
+
+html tarafina template diyecegiz.
+
+String Interpolution : 
+Sizin vue.js içerisinde tanımladıgınız bir key'i, bir property'i html tarafında kullanmanızı saglıyor.
+String interpolution, text content'inin oldugu yerlerde calisir.
+
+vue.js'den template kontrol etmek için kullanacagımız birinci yol string inturpolution
 
 */
