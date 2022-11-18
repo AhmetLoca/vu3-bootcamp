f7516b500a0a83f688fe6a90fb8e3a8fdd90baf9
ahmet <locaahmet@gmail.com>
2022-11-18 16:33:46 +0300

    reactivity-examples

---
+++ 4-reactivity-examples/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      search: "",
+      itemList: ["elma", "armut", "kiraz", "straberry"],
+      //filteredList: [],
+    };
+  },
+  methods: {
+    searchList() {
+      //this.filteredList = this.itemList.filter((i) => i.includes(this.search));
+    },
+  },
+  computed: {
+    filteredList() {
+      return this.itemList.filter((i) => i.includes(this.search));
+    },
+  },
+});
+
+app.mount("#app");
