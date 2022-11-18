c198d5e5aafca9966b7fbbb2c8b4c9f1085b4832
ahmet <locaahmet@gmail.com>
2022-11-19 00:32:04 +0300

    conditions

---
+++ 7-conditions/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showContainer: false,
+      counter: 0,
+    };
+  },
+  computed: {
+    counterBoxClass() {
+      return {
+        "bg-success": this.counter > 0,
+        "bg-danger": this.counter < 0,
+        "bg-primary": this.counter == 0,
+      };
+    },
+  },
+});
+
+app.mount("#app");
