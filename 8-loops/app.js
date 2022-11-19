c4b1b9a8acbcd92d714017ff6d1e19a535757906
ahmet <locaahmet@gmail.com>
2022-11-20 01:19:36 +0300

    todo-app with methods and computed

--- 8-loops/app.js
+++ 8-loops/app.js
@@ -1,19 +1,43 @@
 const app = Vue.createApp({
   data() {
     return {
-      showContainer: false,
-      counter: 0,
+      todoList: [
+        { id: 1, text: "Vue bootcamp hafta 1", completed: false },
+        { id: 2, text: "Vue bootcamp hafta 2", completed: false },
+        { id: 3, text: "Vue bootcamp hafta 3", completed: false },
+        { id: 4, text: "Vue bootcamp hafta 4", completed: false },
+        { id: 5, text: "Vue bootcamp hafta 5", completed: false },
+        { id: 6, text: "Vue bootcamp hafta 6", completed: false },
+      ],
     };
   },
+  methods: {
+    addTodo(event) {
+      this.todoList.push({
+        id: new Date().getTime(),
+        text: event.target.value,
+        completed: false,
+      });
+      event.target.value = "";
+    },
+    removeItem(todoItem) {
+      console.log(todoItem);
+      this.todoList = this.todoList.filter((todo) => todo.id !== todoItem.id);
+    },
+  },
   computed: {
-    counterBoxClass() {
-      return {
-        "bg-success": this.counter > 0,
-        "bg-danger": this.counter < 0,
-        "bg-primary": this.counter == 0,
-      };
+    completedItemCount() {
+      return this.todoList.filter((t) => t.completed).length;
+    },
+    unCompletedItemCount() {
+      return this.todoList.filter((t) => !t.completed).length;
     },
   },
 });
-
 app.mount("#app");
+
+/*
+ addTodo(event) {
+      this.todoList.push(event.target.value);
+    },
+*/
