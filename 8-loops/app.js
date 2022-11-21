107e4365a6338ef33246b3ebcb74f1c1e849faf7
ahmet <locaahmet@gmail.com>
2022-11-21 12:13:51 +0300

    basic todo app

--- 8-loops/app.js
+++ 8-loops/app.js
@@ -5,23 +5,19 @@ const app = Vue.createApp({
         { id: 1, text: "Vue bootcamp hafta 1", completed: false },
         { id: 2, text: "Vue bootcamp hafta 2", completed: false },
         { id: 3, text: "Vue bootcamp hafta 3", completed: false },
-        { id: 4, text: "Vue bootcamp hafta 4", completed: false },
-        { id: 5, text: "Vue bootcamp hafta 5", completed: false },
-        { id: 6, text: "Vue bootcamp hafta 6", completed: false },
       ],
     };
   },
   methods: {
     addTodo(event) {
       this.todoList.push({
-        id: new Date().getTime(),
+        id: Date.now(),
         text: event.target.value,
         completed: false,
       });
       event.target.value = "";
     },
     removeItem(todoItem) {
-      console.log(todoItem);
       this.todoList = this.todoList.filter((todo) => todo.id !== todoItem.id);
     },
   },
@@ -35,9 +31,3 @@ const app = Vue.createApp({
   },
 });
 app.mount("#app");
-
-/*
- addTodo(event) {
-      this.todoList.push(event.target.value);
-    },
-*/
