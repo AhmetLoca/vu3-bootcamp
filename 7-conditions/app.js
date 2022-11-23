09995b85f791de3fa3bc92a6f39393f10c9bdf44
ahmet <locaahmet@gmail.com>
2022-11-23 20:00:38 +0300

    conditions and some example

--- 7-conditions/app.js
+++ 7-conditions/app.js
@@ -1,12 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      showContainer: false,
+      // showContainer: false,
       counter: 0,
     };
   },
+  methods: {
+    //   onOffContainer() {
+    //     this.showContainer = !this.showContainer;
+    //   },
+    increase() {
+      this.counter = this.counter + 1;
+    },
+    decrease() {
+      this.counter = this.counter - 1;
+    },
+  },
+
+  // buttonOpenClose() {
+  //   return {
+  //     "btn btn-sm btn-primary": !this.showContainer,
+  //     "btn btn-sm btn-danger": this.showContainer,
+  //   };
+  // },
   computed: {
-    counterBoxClass() {
+    colorCounter() {
       return {
         "bg-success": this.counter > 0,
         "bg-danger": this.counter < 0,
