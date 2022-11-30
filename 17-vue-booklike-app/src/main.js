b7c4c217a15016c0d35c0a87b72169ca92017c19
ahmet <locaahmet@gmail.com>
2022-12-01 00:57:56 +0300

    vue-booklike-app

--- 17-vue-booklike-app/src/main.js
+++ 17-vue-booklike-app/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/style.css";
+
+import appHeader from "@/components/Shared/appHeader.vue";
 
 const app = createApp(App);
+app.component("appHeader", appHeader);
 app.use(router);
 app.mount("#app");
