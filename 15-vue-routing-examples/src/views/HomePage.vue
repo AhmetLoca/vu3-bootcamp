b835885c90f8ee84507860e78f56f25c598a14c2
ahmet <locaahmet@gmail.com>
2022-11-30 14:39:57 +0300

    bookmark list app

--- 15-vue-routing-examples/src/views/HomePage.vue
+++ 15-vue-routing-examples/src/views/HomePage.vue
@@ -1,26 +1,86 @@
 <template>
-  <table class="table table-striped table-hover">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Title</th>
-        <th scope="col">URL</th>
-        <th scope="col">Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Vue3 Dokümantasyon</td>
-        <td>https://v3.vuejs.org</td>
-        <td><button class="btn btn-sm btn-danger">Delete</button></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="card border p-2">
+    <div class="d-flex justify-content-end">
+      <button
+        class="btn btn-primary btn-sm"
+        @click="$router.push({ name: 'NewBookmark' })"
+      >
+        + New Add
+      </button>
+    </div>
+
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">URL</th>
+          <th scope="col">Description</th>
+          <th scope="col">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(bookmark, index) in bookmarkList" :key="bookmark.id">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ bookmark.title }}</td>
+          <td>
+            <a :href="bookmark.url" target="_blank">{{ bookmark.url }}</a>
+          </td>
+          <td>
+            {{ bookmark.description }}
+          </td>
+          <td>
+            <button
+              @click="deleteBookmark(bookmark)"
+              class="btn btn-sm btn-danger"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      bookmarkList: [],
+    };
+  },
+  created() {
+    this.$appAxios.get("/bookmarks").then((bookmarks_list_response) => {
+      this.bookmarkList = bookmarks_list_response.data || [];
+    });
+  },
+  methods: {
+    deleteBookmark(bookmark) {
+      //console.log("bookmark :>>", bookmark);
+      this.$appAxios
+        .delete(`/bookmarks/${bookmark.id}`)
+        .then((delete_response) => {
+          if (delete_response.status === 200) {
+            this.bookmarkList = this.bookmarkList.filter(
+              (i) => i.id !== bookmark.id
+            );
+            /* bookmarkList içerisinde olan arkadaslardan id'si
+            benim sildigim arkadastan farklı olanları bana getir. o haric
+            hepsini getirecegi için silmiş olacak. */
+          }
+        });
+    },
+  },
+};
+
+/* 
+routing ile beraber life cycle hook'unu kullanarak sayfalar arası 
+istek atıp veriyi çekmeyi gördük.
+Hem de axios'u tekrardan görüp 2 farklı şey yaptık. 1. custom acios instance
+2.global olarak axios'un tanımlanması.
+
+*/
 </script>
 
 <style></style>
--- 15-vue-routing-examples/src/views/NewBookmark.vue
+++ 15-vue-routing-examples/src/views/NewBookmark.vue
@@ -1,13 +1,73 @@
 <template>
-  <h3>New</h3>
+  <div class="form-areal card border p-2">
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input
+        v-model="userData.title"
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="ahmetloca.."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="url" class="form-label">URL</label>
+      <input
+        v-model="userData.url"
+        type="text"
+        class="form-control"
+        id="url"
+        placeholder="https://..."
+      />
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label">Açiklama</label>
+      <textarea
+        v-model="userData.description"
+        class="form-control"
+        placeholder="This is..."
+        id="description"
+      ></textarea>
+    </div>
+
+    <div class="d-flex justify-content-end align-items-center">
+      <button
+        class="btn btn-sm btn-danger me-2"
+        @click="$router.push({ name: 'HomePage' })"
+      >
+        Cancel
+      </button>
+      <button class="btn btn-sm btn-primary" @click="onSave">Save</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      userData: {
+        title: null,
+        url: null,
+        description: null,
+        /* endpoint'im benden bu bilgilerin tamamını istiyor. */
+      },
+    };
+  },
+  methods: {
+    resetData() {
+      Object.keys(this.userData).forEach((key) => (this.userData[key] = null));
+    },
+    onSave() {
+      console.log(this.userData);
+      this.$appAxios.post("/bookmarks", this.userData).then((save_response) => {
+        console.log("save_response", save_response);
+        this.resetData();
+        this.$router.push("/");
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
